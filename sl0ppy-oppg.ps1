61a1d1bcde1463231be5cb1cf80f88626c7f70df
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-08-13 20:30:04 +0200

    Update sl0ppy-oppg.ps1

--- sl0ppy-oppg.ps1
+++ sl0ppy-oppg.ps1
@@ -1,1 +1,1 @@
-Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\5,encoding\5,encoding\5'
+Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\5,encoding\5,encoding\5,clip'

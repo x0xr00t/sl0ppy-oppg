bd2f2557661e523ac5bfcc9335f21df9dbb7922d
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-08-13 22:14:31 +0200

    Update sl0ppy-oppg.ps1

--- sl0ppy-oppg.ps1
+++ sl0ppy-oppg.ps1
@@ -24,9 +24,9 @@ function Show-Menu {
     Write-Host "4: Press '4' For Compression+BXOR+Output."
     Write-Host "5: Press '5' For Dubble-Compression+BXOR+Output."
     Write-Host "6: Press '6' For Tripple-Compression+BXOR+Output."
-    Write-Host "4: Press '7' For Compression+Octal+Output."
-    Write-Host "5: Press '8' For Dubble-Compression+Octal+Output."
-    Write-Host "6: Press '9' For Tripple-Compression+Octal+Output."   
+    Write-Host "7: Press '7' For Compression+Octal+Output."
+    Write-Host "8: Press '8' For Dubble-Compression+Octal+Output."
+    Write-Host "9: Press '9' For Tripple-Compression+Octal+Output."   
     Write-Host "Q: Press 'Q' To Quit."
 }
 
@@ -55,13 +55,13 @@ do
     'You chose option #6'
        Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\6,compress\1,encoding\6,compress\1,encoding\6,out C:\users\x0\Desktop\output.ps1'
     } '7' {
-    'You chose option #4'
+    'You chose option #7'
 	Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\3,encoding\3,encoding\3,out C:\users\x0\Desktop\output.ps1'
     } '8' {
-    'You chose option #5'
+    'You chose option #8'
 	Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\3,compress\1,encoding\3,encoding\3,out C:\users\x0\Desktop\output.ps1'
     } '9' {
-    'You chose option #6'
+    'You chose option #9'
        Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\3,compress\1,encoding\3,compress\1,encoding\3,out C:\users\x0\Desktop\output.ps1'
     }    
     }

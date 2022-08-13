0b35c9c123c8bab8076d37696c658172e8f23f8e
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-08-13 21:48:18 +0200

    Update sl0ppy-oppg.ps1

--- sl0ppy-oppg.ps1
+++ sl0ppy-oppg.ps1
@@ -21,6 +21,9 @@ function Show-Menu {
     Write-Host "1: Press '1' For compression+SecureString+Output."
     Write-Host "2: Press '2' For Dubble-Compression+SecureString+Output."
     Write-Host "3: Press '3' For Tripple-Compression+SecureString+Output."
+    Write-Host "4: Press '1' For compression+BXOR+Output."
+    Write-Host "5: Press '2' For Dubble-Compression+BXOR+Output."
+    Write-Host "6: Press '3' For Tripple-Compression+BXOR+Output."        
     Write-Host "Q: Press 'Q' To Quit."
 }
 
@@ -39,6 +42,16 @@ do
     } '3' {
       'You chose option #3'
 	  Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\5,compress\1,encoding\5,compress\1,encoding\5,out C:\users\x0\Desktop\output.ps1'
+    }
+    '4' {
+    'You chose option #1'
+	Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\6,encoding\6,encoding\6,out C:\users\x0\Desktop\output.ps1'
+    } '5' {
+    'You chose option #2'
+	Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\6,compress\1,encoding\6,encoding\6,out C:\users\x0\Desktop\output.ps1'
+    } '6' {
+      'You chose option #3'
+	  Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\6,compress\1,encoding\6,compress\1,encoding\6,out C:\users\x0\Desktop\output.ps1'
     }
     }
     pause

ddfb0eb954919dd8f1cac4ec65b7e98ca4aaaaa2
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-09-11 19:14:16 -0400

    Update sl0ppy-oppg.ps1

--- sl0ppy-oppg.ps1
+++ sl0ppy-oppg.ps1
@@ -9,10 +9,7 @@
 # place files under C:\
 # add exlusion path C:\sl0ppy-oppg
 #
-# gen obfuscated payload and copy to clipboard... without output to file 
-#Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\5,encoding\5,encoding\5,clip'
-# gen obfuscated payload with output to file
-#Import-Module .\Invoke-Obfuscation.psd1; Invoke-Obfuscation -scriptpath {C:\users\x0\Desktop\sl0ppy-winvasion\1.ps1} -command 'compress\1,encoding\5,encoding\5,encoding\5,out C:\users\x0\Desktop\output.ps1' 
+
 mkdir C:\sl0ppy-payload
 function Show-Menu {
     param (
